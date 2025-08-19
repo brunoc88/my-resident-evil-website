e4e7f44654d4d23e0031ce763092357f1b28c3af
brunoc88 <brunocerutti88@gmail.com>
2025-08-19 13:55:58 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -9,7 +9,7 @@ export const setLogoutCallback = (callback) => {
 // Instancia central de axios
 const api = axios.create({
   // baseURL: 'http://localhost:3000/',
-  baseURL: 'https://umbrellas-a.onrender.com/',
+  baseURL: '/',
 })
 
 // Interceptor de respuesta
