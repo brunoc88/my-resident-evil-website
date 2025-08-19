f48b36da3574a50278a5060b752c7f1bb0f64b9d
brunoc88 <brunocerutti88@gmail.com>
2025-08-19 14:17:27 -0300

    s

--- backend/app.js
+++ backend/app.js
@@ -44,10 +44,6 @@ app.use('/user', userRouter)
 app.use('/personaje', personajeRouter)
 app.use('/denuncias', denunciasRouter)
 
-// SPA fallback
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-})
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
