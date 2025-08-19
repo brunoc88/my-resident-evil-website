aeb03b37a041399957b1d611f587310b94642166
brunoc88 <brunocerutti88@gmail.com>
2025-08-19 13:58:21 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const app = require('./app')
-const { PORT } = require('./utils/config')
+//const { PORT } = require('./utils/config')
 const logger = require('./utils/loggers')
-
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     logger.info(`Escuchando puerto: ${PORT}`)
 })
